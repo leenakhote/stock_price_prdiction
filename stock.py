29dbb197ae246997172539bad7d391c212ab1704
Leena Khote <leenakhote23@gmail.com>
2017-12-18 11:56:37 +0530

    Update stock.py

--- stock.py
+++ stock.py
@@ -2,7 +2,6 @@ import csv
 import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
-matplotlib.use('TKagg')
 
 dates = []
 prices = []
@@ -12,24 +11,23 @@ def get_data(filename):
         csvFileReader = csv.reader(csvfile)
         next(csvFileReader)
         for row in csvFileReader:
-            dates.append(int(row[0].split('-')[0]))
+            dates.append(int(row[0].split('-')[2]))
             prices.append(float(row[1]))
     return
-
 
 def predict_price(dates, prices, x):
     dates = np.reshape(dates,(len(dates), 1))
 
     svr_len = SVR(kernel= 'linear', C=1e3)
     svr_poly = SVR(kernel= 'poly', C=1e3, degree = 2)
-    svr_rbf = SVVR(kernel= 'rbf', C=1e3, gamma=0.1)
-    svr_lin.fit(dates, prices)
+    svr_rbf = SVR(kernel= 'rbf', C=1e3, gamma=0.1)
+    svr_len.fit(dates, prices)
     svr_poly.fit(dates, prices)
     svr_rbf.fit(dates, prices)
 
     plt.scatter(dates,prices, color ='black', label='Data')
     plt.plot(dates, svr_rbf.predict(dates), color = 'red', label = 'RBF model')
-    plt.plot(dates, svr_lin.predict(dates), color = 'green', label = 'Linear model')
+    plt.plot(dates, svr_len.predict(dates), color = 'green', label = 'Linear model')
     plt.plot(dates, svr_poly.predict(dates), color = 'blue', label = 'Polynomial model')
 
     plt.xlabel('Date')
@@ -38,10 +36,10 @@ def predict_price(dates, prices, x):
     plt.legend()
     plt.show()
 
-    return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+    return svr_rbf.predict(x)[0], svr_len.predict(x)[0], svr_poly.predict(x)[0]
 
-    get_data('/home/stylabs/dev/NSEI.csv')
+get_data('/home/leena-stylabs/dev/darwin/stock_price_prdiction/NSEI.csv')
 
-    predicted_price = predict_price(dates, prices, 29)
+predicted_price = predict_price(dates, prices, 29)
 
-    print predicted_price
+print predicted_price
